91bc099711cf6c9447f3db85e2fadd30000aa3e7
martineizayaga <martineizayaga@gmail.com>
2018-01-08 02:48:20 -0500

    fixed welcome, nav, and now styles

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,14 +1,13 @@
 <header id="masthead">
-    <h3 id="masthead-title"><a href="/">@me393</a></h3>
+    <h3 id="masthead-title"><a href="/">Martín Eizayaga</a></h3>
     <nav id="web-nav">
         <ul>
-        	<li><a href="/now">Now</a></li>
             <li><a href="/portfolio">Portfolio</a></li>
             <li><a href="/blog">Blog</a></li>
             <li><a href="https://drive.google.com/file/d/1ehGh5SzNzs1e4EEe-neRJdAItf0zCa4d/view?usp=sharing">CV</a></li>
             @if (Auth::guest())
             @else
-            	<li>
+                <li>
                     <a href="{{ url('/logout') }}"
                         onclick="event.preventDefault();
                                  document.getElementById('logout-form').submit();">
